5ad80aafe0b4f1bbace34bfe668661098fe74ab9
Yuu <chaerieyoo@protonmail.com>
2022-11-05 20:46:49 +0100

    Fertigstellen der groben GUI inkl. paddings An die Output-Labels werden dann die Ausgaben aus der Datenbank gesendet werden
    Nächster Schritt ist das verknüpfen dieser Class mit der Datenverarbeitungsclass
    -> Anschließend werde ich dann einfache MySQL statements casten und durch Output-Labels ausgeben

--- GUI.py
+++ GUI.py
@@ -9,9 +9,9 @@ class GUI():
         # die ggf. vom Fenster bestimmt sind; Da Du nur 1x Fenster brauchst Latte
         self.root = Tk()
         self.root.title("Organize your beans!")
-        self.root.geometry("500x500")
+        self.root.geometry("700x500")
 
-        self.main_window = ttk.Frame(self.root, padding="3 3 12 12")
+        self.main_window = ttk.Frame(self.root, padding="20")
         self.main_window.grid(column=0, row=0, sticky=(N, W, E, S))
 
         self.root.columnconfigure(0, weight=1)
@@ -23,12 +23,59 @@ class GUI():
 
         #  Erstellen der Widgets, der übersichtlichkeitshalber
 
-        # Buttons
+        # Labels
+        item_name = ttk.Label(self.main_window, text="Artikelname:")
+        item_name.grid(column=0, row=1, pady=10)
 
+        item_cat = ttk.Label(self.main_window, text="Artikelcat:")
+        item_cat.grid(column=0, row=2, pady=10)
+
+        item_kaufdatum = ttk.Label(self.main_window, text="Kaufdatum YYYY/MM/DD:")
+        item_kaufdatum.grid(column=0, row=3, pady=10)
+
+        item_menge = ttk.Label(self.main_window, text="Menge:")
+        item_menge.grid(column=2, row=1, pady=10)
+
+        item_size = ttk.Label(self.main_window, text="Groessenangabe:")
+        item_size.grid(column=2, row=2, pady=10)
+
+        output = ttk.Label(self.main_window, text="OUTPUT")
+        output.grid(column=0, row=5, pady=10)
+
+        mhd_gekuehlt = ttk.Label(self.main_window, text="Haltbarkeitsdatum gekuehlt:")
+        mhd_gekuehlt.grid(column=0, row=6, pady=10)
+
+        mhd_ungekuehlt = ttk.Label(self.main_window, text="Haltbarkeitsdatum ungekuehlt:")
+        mhd_ungekuehlt.grid(column=0, row=7, pady=10)
+
+        output_mhd_gekuehlt = ttk.Label(self.main_window, text="Insert Data here")
+        output_mhd_gekuehlt.grid(column=3, row=6, pady=10)
+
+        output_mhd_ungekuehlt = ttk.Label(self.main_window, text="Insert Data here")
+        output_mhd_ungekuehlt.grid(column=3, row=7, pady=10)
 
         # Entries
-        item_entry = ttk.Entry(self.main_window)
-        item_entry.grid(column=1, row=0)
+        item_name_entry = ttk.Entry(self.main_window)
+        item_name_entry.grid(column=1, row=1, pady=10)
 
+        item_cat_entry = ttk.Entry(self.main_window)
+        item_cat_entry.grid(column=1, row=2, pady=10)
+
+        item_kaufdatum_entry = ttk.Entry(self.main_window)
+        item_kaufdatum_entry.grid(column=3, row=3, pady=10)
+
+        item_menge_entry = ttk.Entry(self.main_window)
+        item_menge_entry.grid(column=3, row=1, pady=10)
+
+        items_size_entry = ttk.Entry(self.main_window)
+        items_size_entry.grid(column=3, row=2, pady=10)
+
+        # Buttons
+
+        btn_add = Button(self.main_window, text="Hinzufügen")  # command=
+        btn_add.grid(column=0, row=4, pady=10)
+
+        btn_del = Button(self.main_window, text="Löschen")
+        btn_del.grid(column=1, row=4, pady=10)
 
         self.root.mainloop()  # Ohne das mainloop startet die Erstellung des Fensters nicht
