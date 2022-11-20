cec2ceb80ecd174be885af81e66b0b645ec84058
Yuu <chaerieyoo@protonmail.com>
2022-11-20 18:46:56 +0100

    Trennen der .grid()-calls vom Initialisieren der Widgets Formatierung nach aktuellem PEP8 und inkl. aktueller PycharmV

--- GUI.py
+++ GUI.py
@@ -2,14 +2,8 @@ import tkinter
 from tkinter import *
 from tkinter import ttk
 
-import process_db
-from process_db import *
 
-
-# ttk ist eine Art "neues" Modul um Widgets zu erstellen
-
-
-class GUI():
+class GUI:
     '''Visuals der Desktop-Anwendung'''
 
     def __init__(self):
@@ -20,7 +14,7 @@ class GUI():
         self.root.geometry("700x500")
 
         self.main_window = ttk.Frame(self.root, padding="20")
-        self.main_window.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.main_window.grid(column=0, row=0, sticky="N, W, E, S")
 
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(0, weight=1)
@@ -32,76 +26,60 @@ class GUI():
         #  Erstellen der Widgets, der übersichtlichkeitshalber
 
         # Labels
-        self.item_name_label = ttk.Label(self.main_window, text="Artikelname:").grid(column=0,
-                                                                                     row=1,
-                                                                                     pady=10)
+        self.item_name_label = ttk.Label(self.main_window, text="Artikelname:")
+        self.item_name_label.grid(column=0, row=1, pady=10)
 
-        self.item_cat_label = ttk.Label(self.main_window, text="Artikelcat:").grid(column=0,
-                                                                                   row=2,
-                                                                                   pady=10)
+        self.item_cat_label = ttk.Label(self.main_window, text="Artikelcat:")
+        self.item_cat_label.grid(column=0, row=2, pady=10)
 
-        self.item_purchase_date_label = ttk.Label(self.main_window, text="Kaufdatum YYYY/MM/DD:").grid(column=0,
-                                                                                                       row=3,
-                                                                                                       pady=10)
+        self.item_purchase_date_label = ttk.Label(self.main_window, text="Kaufdatum YYYY/MM/DD:")
+        self.item_purchase_date_label.grid(column=0, row=3, pady=10)
 
-        self.item_amount_label = ttk.Label(self.main_window, text="Menge:").grid(column=2,
-                                                                                 row=1,
-                                                                                 pady=10)
+        self.item_amount_label = ttk.Label(self.main_window, text="Menge:")
+        self.item_amount_label.grid(column=2, row=1, pady=10)
 
-        self.item_size_label = ttk.Label(self.main_window, text="Groessenangabe:").grid(column=2,
-                                                                                        row=2,
-                                                                                        pady=10)
+        self.item_size_label = ttk.Label(self.main_window, text="Groessenangabe:")
+        self.item_size_label.grid(column=2, row=2, pady=10)
 
-        self.output_label = ttk.Label(self.main_window, text="OUTPUT").grid(column=0,
-                                                                            row=5,
-                                                                            pady=10)
+        self.output_label = ttk.Label(self.main_window, text="OUTPUT")
+        self.output_label.grid(column=0, row=5, pady=10)
 
-        self.mhd_gekuehlt_label = ttk.Label(self.main_window, text="Haltbarkeitsdatum gekuehlt:").grid(column=0,
-                                                                                                       row=6,
-                                                                                                       pady=10)
+        self.mhd_gekuehlt_label = ttk.Label(self.main_window, text="Haltbarkeitsdatum gekuehlt:")
+        self.mhd_gekuehlt_label.grid(column=0, row=6, pady=10)
 
-        self.mhd_ungekuehlt_label = ttk.Label(self.main_window, text="Haltbarkeitsdatum ungekuehlt:").grid(column=0,
-                                                                                                           row=7,
-                                                                                                           pady=10)
+        self.mhd_ungekuehlt_label = ttk.Label(self.main_window, text="Haltbarkeitsdatum ungekuehlt:")
+        self.mhd_gekuehlt_label.grid(column=0, row=7, pady=10)
 
-        self.output_mhd_gekuehlt_label = ttk.Label(self.main_window, text="Insert Data here").grid(column=3,
-                                                                                                   row=6,
-                                                                                                   pady=10)
+        self.output_mhd_gekuehlt_label = ttk.Label(self.main_window, text="Insert Data here")
+        self.output_mhd_gekuehlt_label.grid(column=3, row=6, pady=10)
 
-        self.output_mhd_ungekuehlt_label = ttk.Label(self.main_window, text="Insert Data here").grid(column=3,
-                                                                                                     row=7,
-                                                                                                     pady=10)
+        self.output_mhd_ungekuehlt_label = ttk.Label(self.main_window, text="Insert Data here")
+        self.output_mhd_ungekuehlt_label.grid(column=3, row=7, pady=10)
 
         # Entries
         self.item_name = tkinter.StringVar(self.root)
-        self.item_name_entry = ttk.Entry(self.main_window, textvariable=self.item_name).grid(column=1,
-                                                                                             row=1,
-                                                                                             pady=10)
+        self.item_name_entry = ttk.Entry(self.main_window, textvariable=self.item_name)
+        self.item_name_entry.grid(column=1, row=1, pady=10)
 
         self.item_cat = tkinter.StringVar(self.root)
-        self.item_cat_entry = ttk.Entry(self.main_window, textvariable=self.item_cat).grid(column=1,
-                                                                                           row=2,
-                                                                                           pady=10)
+        self.item_cat_entry = ttk.Entry(self.main_window, textvariable=self.item_cat)
+        self.item_cat_entry.grid(column=1, row=2, pady=10)
 
         self.item_purchase_date = tkinter.StringVar(self.root)
-        self.item_purchase_data_entry = ttk.Entry(self.main_window, textvariable=self.item_purchase_date).grid(column=3,
-                                                                                                               row=3,
-                                                                                                               pady=10)
+        self.item_purchase_data_entry = ttk.Entry(self.main_window, textvariable=self.item_purchase_date)
+        self.item_purchase_data_entry.grid(column=3, row=3, pady=10)
+
         self.item_amount = tkinter.StringVar(self.root)
-        self.item_amount_entry = ttk.Entry(self.main_window, textvariable=self.item_amount).grid(column=3,
-                                                                                                 row=1,
-                                                                                                 pady=10)
+        self.item_amount_entry = ttk.Entry(self.main_window, textvariable=self.item_amount)
+        self.item_amount_entry.grid(column=3, row=1, pady=10)
 
         self.item_size = tkinter.StringVar(self.root)
-        self.items_size_entry = ttk.Entry(self.main_window, textvariable=self.item_size).grid(column=3,
-                                                                                              row=2,
-                                                                                              pady=10)
-
+        self.items_size_entry = ttk.Entry(self.main_window, textvariable=self.item_size)
+        self.items_size_entry.grid(column=3, row=2, pady=10)
         # Buttons
 
-        self.btn_add = Button(self.main_window, text="Hinzufügen", command=self.fetch_data).grid(column=0,
-                                                                                                 row=4,
-                                                                                                 pady=10)
+        self.btn_add = Button(self.main_window, text="Hinzufügen", command=self.fetch_data)
+        self.btn_add.grid(column=0, row=4, pady=10)
 
         self.root.mainloop()  # Ohne das mainloop startet die Erstellung des Fensters nicht
 
@@ -112,7 +90,6 @@ class GUI():
               self.item_name.get(),
               self.item_purchase_date.get(),
               self.item_size.get())
-
 
 
 window = GUI()
