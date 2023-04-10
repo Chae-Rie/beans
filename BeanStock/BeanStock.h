7ce6cd29bc8152827b93fd14decc9685ce27c85b
Chae-Rie <yoospam@protonmail.com>
2023-04-10 13:58:15 +0200

    change: anpassen der gitignore change: ändern des dialogs auf x64 add: hinzufügen eines closes fensters

---
+++ BeanStock/BeanStock.h
@@ -0,0 +1,32 @@
+
+// BeanStock.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CBeanStockApp:
+// See BeanStock.cpp for the implementation of this class
+//
+
+class CBeanStockApp : public CWinApp
+{
+public:
+	CBeanStockApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CBeanStockApp theApp;
---
+++ BeanStock/BeanStockDlg.h
@@ -0,0 +1,33 @@
+
+// BeanStockDlg.h : header file
+//
+
+#pragma once
+
+
+// CBeanStockDlg dialog
+class CBeanStockDlg : public CDialogEx
+{
+// Construction
+public:
+	CBeanStockDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_BEANSTOCK_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
